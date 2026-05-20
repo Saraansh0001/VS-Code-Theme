518ad6522432abad21d93f40f4eb633ee63453ae
Saraansh <gandhisaraansh@gmail.com>
2026-05-20 16:55:55 +0530

    ferrari theme done

--- f1-theme-extension/src/extension.ts
+++ f1-theme-extension/src/extension.ts
@@ -1,26 +1,88 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const LAP_KEY = 'scuderiaFerrari.lapCount';
+const PRANCING_HORSE_COMMAND = 'scuderiaFerrari.prancingHorseMode';
+const ADD_LAP_COMMAND = 'scuderiaFerrari.addRaceLap';
+const RESET_LAPS_COMMAND = 'scuderiaFerrari.resetRaceLapCounter';
+const PIT_STOP_COMMAND = 'scuderiaFerrari.startPitStopTimer';
+
+let statusBarItem: vscode.StatusBarItem;
+let lapCount = 0;
+let pitStopTimer: NodeJS.Timeout | undefined;
+
 export function activate(context: vscode.ExtensionContext) {
+	lapCount = context.globalState.get<number>(LAP_KEY, 0);
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "f1-theme-extension" is now active!');
+	statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
+	statusBarItem.name = 'Scuderia Ferrari Race Control';
+	statusBarItem.command = PRANCING_HORSE_COMMAND;
+	statusBarItem.tooltip = new vscode.MarkdownString('$(dashboard) PRANCING HORSE MODE\n\nClick to add a racing lap.');
+	updateStatusBar();
+	statusBarItem.show();
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('f1-theme-extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from f1-theme-extension!');
+	const prancingHorseMode = vscode.commands.registerCommand(PRANCING_HORSE_COMMAND, async () => {
+		await addLap(context);
+		vscode.window.showInformationMessage(`PRANCING HORSE MODE: Lap ${lapCount} logged. Forza Ferrari.`);
 	});
 
-	context.subscriptions.push(disposable);
+	const addRaceLap = vscode.commands.registerCommand(ADD_LAP_COMMAND, async () => {
+		await addLap(context);
+		vscode.window.setStatusBarMessage(`Scuderia Ferrari lap ${lapCount} recorded`, 2500);
+	});
+
+	const resetRaceLapCounter = vscode.commands.registerCommand(RESET_LAPS_COMMAND, async () => {
+		lapCount = 0;
+		await context.globalState.update(LAP_KEY, lapCount);
+		updateStatusBar();
+		vscode.window.showInformationMessage('Race lap counter reset. Fresh tyres, clear track.');
+	});
+
+	const startPitStopTimer = vscode.commands.registerCommand(PIT_STOP_COMMAND, () => {
+		if (pitStopTimer) {
+			clearTimeout(pitStopTimer);
+		}
+
+		const startedAt = Date.now();
+		vscode.window.setStatusBarMessage('Pit stop timer running: target 2.6s', 2600);
+
+		pitStopTimer = setTimeout(() => {
+			const elapsedSeconds = ((Date.now() - startedAt) / 1000).toFixed(2);
+			vscode.window.showInformationMessage(`Pit stop complete in ${elapsedSeconds}s. Box, box, launch.`);
+			pitStopTimer = undefined;
+		}, 2600);
+	});
+
+	context.subscriptions.push(
+		statusBarItem,
+		prancingHorseMode,
+		addRaceLap,
+		resetRaceLapCounter,
+		startPitStopTimer,
+		{
+			dispose: () => {
+				if (pitStopTimer) {
+					clearTimeout(pitStopTimer);
+				}
+			}
+		}
+	);
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+	if (pitStopTimer) {
+		clearTimeout(pitStopTimer);
+		pitStopTimer = undefined;
+	}
+}
+
+async function addLap(context: vscode.ExtensionContext) {
+	lapCount += 1;
+	await context.globalState.update(LAP_KEY, lapCount);
+	updateStatusBar();
+}
+
+function updateStatusBar() {
+	statusBarItem.text = `$(dashboard) 🏎️ Ferrari Lap ${lapCount}`;
+	statusBarItem.backgroundColor = new vscode.ThemeColor('statusBarItem.prominentBackground');
+	statusBarItem.color = new vscode.ThemeColor('statusBarItem.prominentForeground');
+}
